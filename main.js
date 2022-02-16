0b186a79360e2e52ecf4f0f48b834df43e645f3b
mil2ne <show3070@gmail.com>
2022-02-17 07:43:34 +0900

    reverse practice

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// .push() .unshift()
-// 원본 수정됨 주의
+// reverse
+// 원본수정됨 주의
 
 const numbers = [1,2,3,4]
 const fruits = ['Apple', 'Banana', 'Cherry']
 
-numbers.push(5)
-console.log(numbers) // 가장 뒤쪽에 추가
+numbers.reverse()
+fruits.reverse()
 
-numbers.unshift(0)
-console.log(numbers) // 가장 앞쪽에 추가
+console.log(numbers)
+console.log(fruits)
