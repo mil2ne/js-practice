376f186c68ed685f21cad278eaab45d12b04f01b
mil2ne <show3070@gmail.com>
2022-02-17 07:50:41 +0900

    splice practice

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// reverse
+// splice()
 // 원본수정됨 주의
 
 const numbers = [1,2,3,4]
 const fruits = ['Apple', 'Banana', 'Cherry']
 
-numbers.reverse()
-fruits.reverse()
+numbers.splice(2,2) 
+console.log(numbers) // 1,2
 
-console.log(numbers)
-console.log(fruits)
+numbers.splice(1,0,999)
+console.log(numbers) // 1,999,2
+
+numbers.splice(2,1,99)
+console.log(numbers) // 1,999,99
+
+fruits.splice(2,0,'Orange')
+console.log(fruits) // 'Apple', 'Banana', 'Orange', 'Cherry'
