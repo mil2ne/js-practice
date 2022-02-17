450f4c14ef91de8ea6c6a86135b7073a7fcd2f48
mil2ne <show3070@gmail.com>
2022-02-18 07:38:02 +0900

    copy pracitce - 2

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// 얕은복사 (Shallow copy) , 깊은복사 (Deep copy) - 1
+// 얕은복사 (Shallow copy) , 깊은복사 (Deep copy) - 2
 
 const user = {
   name: 'Heropy',
   age: 85,
   emails: ['thesecon@gmail.com']
 }
-const copyUser = Object.assign({}, user)
+const copyUser = {...user}
 console.log(copyUser === user) // false
 
 user.age = 22
@@ -14,3 +14,8 @@ console.log('copyUser', copyUser)  // {name: 'Heropy', age: 85, emails: Array(1)
 
 console.log('------')
 console.log('------')
+
+user.emails.push('mil2ne@gmail.com')
+console.log(user.emails === copyUser.emails) // true
+console.log('user', user) // {name: 'Heropy', age: 22, emails: ['thesecon@gmail.com', 'mil2ne@gmail.com']}
+console.log('copyUser', copyUser) // {name: 'Heropy', age: 85, emails: ['thesecon@gmail.com', 'mil2ne@gmail.com']}
