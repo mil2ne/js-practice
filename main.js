4eb38e91155b5feb3983f114ae3675f75bb0f075
mil2ne <show3070@gmail.com>
2022-02-18 06:55:46 +0900

    전개연산자

--- main.js
+++ main.js
@@ -1,34 +1,14 @@
-// 구조 분해 할당 (Destructuring assignment)
-// 비구조화 할당
+// 전개 연산자 (Spread) 기본
 
-const user = {
-  name: 'Heropy',
-  age: 85,
-  email: 'thesecon@gmail.com',
-  address: 'USA'
+const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
+console.log(fruits) //  ['Apple', 'Banana', 'Cherry']
+console.log(...fruits) // Apple Banana Cherry
+
+function toObject(a,b,c) {
+  return {
+    a: a,
+    b: b,
+    c: c,
+  }
 }
-
-const { name: heropy, age, email, address = 'Korea'} = user
-// E.g user.address
-// address 가 기본설정되어 있으면 기본 적용한 Korea 는 무시
-
-// 변수명을 : 기호를 통해서 임의 변경 가능
-
-
-console.log(`사용자의 이름은 ${heropy} 입니다.`)
-console.log(`${heropy} 의 나이는 ${age} 입니다.`)
-console.log(`${heropy} 의 이메일 주소는 ${email} 입니다.`)
-console.log(address)
-
-
-const fruits = ['Apple', 'Banana', 'Cherry']
-const [a,b,c,d] = fruits
-console.log(a,b,c,d)
-
-const fruit = ['Apple', 'Banana', 'Cherry']
-const [,f] = fruit
-console.log(f) // Banana
-
-const fruit1 = ['Apple', 'Banana', 'Cherry']
-const [,,g] = fruit1
-console.log(g) // Cherry
+console.log(toObject(...fruits)) // {a: 'Apple', b: 'Banana', c: 'Cherry'}
