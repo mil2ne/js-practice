7f1e55480aa7563cedcbd14dded963fed53e1917
mil2ne <show3070@gmail.com>
2022-02-17 19:58:25 +0900

    Object.assign practice 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 객체 데이터 1(Object.assign)
+// 객체 데이터 2(Object.assign)
 
 const userAge = {
   name: 'mil2ne',
@@ -10,10 +10,10 @@ const userEmail = {
   email: 'mil2ne@gmail.com'
 }
 
-const target = Object.assign(userAge, userEmail)
+const target = Object.assign({}, userAge, userEmail)
 console.log(target) // {name: 'mil2ne', age: 85, email: 'mil2ne@gmail.com'}
-console.log(userAge) // {name: 'mil2ne', age: 85, email: 'mil2ne@gmail.com'}
-console.log(target === userAge) // true
+console.log(userAge) // {name: 'mil2ne', age: 85}
+console.log(target === userAge) // false
 
 const a = {k: 123}
 const b = {k: 123}
