0400e1934a48c20a28cd1946fd90fe7e7b2bc81e
mil2ne <show3070@gmail.com>
2022-02-18 07:25:14 +0900

    데이터불변성 - 2

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// 데이터 불변성 ( immutablility ) - 1
+// 데이터 불변성 ( immutablility ) - 2
 // 원시데이터 : String, Number , Boolean, undefined, null
 // 참조형 데이터 : Object , Array, Function
 
-let a = 1
-let b = 4
-console.log(a,b, a === b) // 1 4 false
+let a = { k: 1 }
+let b = { k: 1 }
+console.log(a, b, a == b) // {k: 1} {k: 1} false
+
+a.k = 7
 b = a
-console.log(a,b, a === b) // 1 1 true
-a = 7
-console.log(a,b, a === b) // 7 1 false
-let c = 1
-console.log(b,c, b === c) // 1 1 true
+console.log(a, b, a == b) // {k: 7} {k: 7} true
+
+a.k = 2
+console.log(a, b, a == b) // {k: 2} {k: 2} true
+
+let c = b
+console.log(a, b, c, a == c) // {k: 2} {k: 2} {k: 2} true
+
+a.k = 9
+console.log(a, b, c, a == c) // {k: 9} {k: 9} {k: 9} true
