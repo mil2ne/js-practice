6becbc59d34c085d3242ace2eced04c86de1f69b
mil2ne <show3070@gmail.com>
2022-02-17 20:27:22 +0900

    구조분해할당

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-// 객체 데이터 4(Object.keys)
+// 구조 분해 할당 (Destructuring assignment)
+// 비구조화 할당
 
 const user = {
   name: 'Heropy',
   age: 85,
-  email: 'thesecon@gmail.com'
+  email: 'thesecon@gmail.com',
+  address: 'USA'
 }
 
-const keys = Object.keys(user)
-console.log(keys) //  ['name', 'age', 'email']
+const { name: heropy, age, email, address = 'Korea'} = user
+// E.g user.address
+// address 가 기본설정되어 있으면 기본 적용한 Korea 는 무시
 
-console.log(user['email']) // thesecon@gmail.com
+// 변수명을 : 기호를 통해서 임의 변경 가능
 
-const values = keys.map(key => user[key])
-console.log(values) // ['Heropy', 85, 'thesecon@gmail.com']
+
+console.log(`사용자의 이름은 ${heropy} 입니다.`)
+console.log(`${heropy} 의 나이는 ${age} 입니다.`)
+console.log(`${heropy} 의 이메일 주소는 ${email} 입니다.`)
+console.log(address)
+
+
+const fruits = ['Apple', 'Banana', 'Cherry']
+const [a,b,c,d] = fruits
+console.log(a,b,c,d)
+
+const fruit = ['Apple', 'Banana', 'Cherry']
+const [,f] = fruit
+console.log(f) // Banana
+
+const fruit1 = ['Apple', 'Banana', 'Cherry']
+const [,,g] = fruit1
+console.log(g) // Cherry
