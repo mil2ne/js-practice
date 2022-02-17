ab23e9b63af203fbe342976661f55907f1b8cb23
mil2ne <show3070@gmail.com>
2022-02-18 07:00:14 +0900

    전개연산자 rest parameter

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-// 전개 연산자 (Spread) 기본
+// 전개 연산자 (Spread) rest parameter
 
 const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
 console.log(fruits) //  ['Apple', 'Banana', 'Cherry']
 console.log(...fruits) // Apple Banana Cherry
 
-function toObject(a,b,c) {
-  return {
-    a: a,
-    b: b,
-    c: c,
-  }
-}
-console.log(toObject(...fruits)) // {a: 'Apple', b: 'Banana', c: 'Cherry'}
+const toObject = (a,b,...c) => ({a,b,c})
+console.log(toObject(...fruits)) // {a: 'Apple', b: 'Banana', c: ['Cherry', 'Orange']}
